ee76b6232e0349e9ec0e90426f2405f1b0b55fc1
bluetie915 <1402711057@qq.com>
2020-01-31 18:07:34 +0800

    重复提交

--- webpack的使用/04-webpack配置vue/src/main.js
+++ webpack的使用/04-webpack配置vue/src/main.js
@@ -26,9 +26,31 @@ document.writeln('你好，小明')
 
 // 5.使用vue进行开发
 import Vue from 'vue'
-const app = new Vue({
+const app = {
+  template: `
+    <div>
+      <h2>{{message}}</h2>
+      <button @click="btnClick">按钮</button>
+      <h2>{{name}}</h2>
+    </div>
+  `,
+  data() {
+    return {
+      message: 'Hello webpack',
+      name: "blueTie"
+    }
+  },
+  methods: {
+    btnClick() {
+
+    }
+  }
+}
+
+new Vue({
   el: '#app',
-  data: {
-    message: 'Hello webpack'
+  template: '<App></App>',
+  components: {
+    App
   }
 })

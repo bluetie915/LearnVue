cc712a6c94e5d0b88866eaa6aaffd55f25f98086
bluetie915 <1402711057@qq.com>
2020-02-02 21:06:10 +0800

    配置路由

---
+++ VueCli/05-learnvuerouter/src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>我是关于</h2>
+    <h2>我是关于内容，哈哈</h2>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "About",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ VueCli/05-learnvuerouter/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>我是主页</h2>
+    <h2>哈哈</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped></style>

5b610e40a0764812b3e456e82eaf14249f7a1493
bluetie915 <1402711057@qq.com>
2020-02-06 23:06:42 +0800

    初识axios

---
+++ 06-VueX和axios/learnaxios/src/main.js
@@ -0,0 +1,72 @@
+import Vue from "vue"
+import App from "./App.vue"
+
+import axios from "axios"
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app")
+
+// 1.axios的基本使用
+// axios({
+//   // 默认get请求
+//   url: "http://123.207.32.32:8000/home/multidata"
+// }).then(res => {
+//   console.log(res)
+// })
+
+// axios({
+//   url: "http://123.207.32.32:8000/home/data",
+//   params: {
+//     type: "pop",
+//     page: 1
+//   }
+// }).then(res => {
+//   console.log(res)
+// })
+
+// 2.axios的并发请求
+axios
+  .all([
+    axios({
+      url: "http://123.207.32.32:8000/home/multidata"
+    }),
+    axios({
+      url: "http://123.207.32.32:8000/home/data",
+      params: {
+        type: "pop",
+        page: 5
+      }
+    })
+  ])
+  .then(res => {
+    console.log(res)
+    console.log(res[0])
+    console.log(res[1])
+  })
+
+// 全局配置，特殊要求也可以写在某个请求里面
+axios.defaults.baseURL = "http://123.207.32.32:8000"
+axios.defaults.timeout = 5000
+
+axios
+  .all([
+    axios({
+      url: "/home/multidata"
+    }),
+    axios({
+      url: "/home/data",
+      params: {
+        type: "pop",
+        page: 5
+      }
+    })
+  ])
+  .then(
+    axios.spread((res1, res2) => {
+      console.log(res1)
+      console.log(res2)
+    })
+  )

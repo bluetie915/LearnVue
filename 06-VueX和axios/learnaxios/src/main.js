daf61088282624d2b8c17a3d555bfa82bb8606dd
bluetie915 <1402711057@qq.com>
2020-02-06 23:34:08 +0800

    处理错误

--- 06-VueX和axios/learnaxios/src/main.js
+++ 06-VueX和axios/learnaxios/src/main.js
@@ -28,45 +28,64 @@ new Vue({
 // })
 
 // 2.axios的并发请求
-axios
-  .all([
-    axios({
-      url: "http://123.207.32.32:8000/home/multidata"
-    }),
-    axios({
-      url: "http://123.207.32.32:8000/home/data",
-      params: {
-        type: "pop",
-        page: 5
-      }
-    })
-  ])
-  .then(res => {
-    console.log(res)
-    console.log(res[0])
-    console.log(res[1])
-  })
+// axios
+//   .all([
+//     axios({
+//       url: "http://123.207.32.32:8000/home/multidata"
+//     }),
+//     axios({
+//       url: "http://123.207.32.32:8000/home/data",
+//       params: {
+//         type: "pop",
+//         page: 5
+//       }
+//     })
+//   ])
+//   .then(res => {
+//     console.log(res)
+//     console.log(res[0])
+//     console.log(res[1])
+//   })
 
-// 全局配置，特殊要求也可以写在某个请求里面
-axios.defaults.baseURL = "http://123.207.32.32:8000"
-axios.defaults.timeout = 5000
+// 3.全局配置进行网络请求
+// axios.defaults.baseURL = "http://123.207.32.32:8000"
+// axios.defaults.timeout = 5000
 
-axios
-  .all([
-    axios({
-      url: "/home/multidata"
-    }),
-    axios({
-      url: "/home/data",
-      params: {
-        type: "pop",
-        page: 5
-      }
-    })
-  ])
-  .then(
-    axios.spread((res1, res2) => {
-      console.log(res1)
-      console.log(res2)
-    })
-  )
+// axios
+//   .all([
+//     axios({
+//       url: "/home/multidata"
+//     }),
+//     axios({
+//       url: "/home/data",
+//       params: {
+//         type: "pop",
+//         page: 5
+//       }
+//     })
+//   ])
+//   .then(
+//     axios.spread((res1, res2) => {
+//       console.log(res1)
+//       console.log(res2)
+//     })
+//   )
+
+// 4.创建对应的axios实例
+const instance1 = axios.create({
+  baseURL: "http://123.207.32.32:8000",
+  timeout: 5000
+})
+
+instance1({
+  url: "/home/multidata"
+}).then(res => {
+  console.log(res)
+})
+instance1({
+  url: "/home/data",
+  params: {
+    type: "pop",
+    age: 2
+  }
+})

f80b5c63859f125c28e14704ae40017464df8940
bluetie915 <1402711057@qq.com>
2020-02-09 22:20:02 +0800

    使用better-scroll重构代码

---
+++ 07-小项目/supermall/src/components/common/scroll/Scroll.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- ref如果绑定在组件中，那么通过this.$refs.refname获取到的是一个组件对象 -->
+  <!-- ref如果绑定在普通的元素中，那么通过this.$refs.refname获取到的是一个元素对象 -->
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+import BScroll from "better-scroll"
+
+export default {
+  name: "Scroll",
+  data() {
+    return {
+      scroll: null
+    }
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper, {})
+  }
+}
+</script>
+<style scoped></style>
